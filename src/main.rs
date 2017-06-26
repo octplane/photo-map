951c9cce8af9456d64eeacf7b86e8c41147755af
Pierre BAILLET <pierre.baillet@datadoghq.com>
2017-06-27 00:17:41 +0200

    Better stuff

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,49 @@ extern crate rexif;
 use std::fs;
 use std::io;
 use std::error::Error;
+use std::collections::HashMap;
 
 use rexif::ExifTag;
+use rexif::TagValue;
 
-fn scan_entries() -> Result<(), io::Error> {
+struct PhotoInContext {
+}
+
+#[derive(Debug)]
+enum CoordOrValue {
+    Coordinate(f64),
+    Orientation(String),
+}
+
+fn scan_entries() -> Result<Vec<PhotoInContext>, io::Error> {
+    let  photos = Vec::new();
     for entry in fs::read_dir("2015-03-06-Gordon-Ramzay-Trianon")? {
         let dir = entry?;
         let pfname = dir.path();
         let fname = pfname.to_str();
         let file_name = fname.unwrap();
+        println!("File: {}", file_name);
 
         match rexif::parse_file(&file_name) {
             Ok(exif) => {
-                let entries: Vec<rexif::ExifEntry> = exif.entries.into_iter().filter(|en| 
+                let entries: HashMap<rexif::ExifTag, CoordOrValue> = exif.entries.into_iter().filter(|en| 
                     en.tag == ExifTag::GPSLatitudeRef ||
                     en.tag == ExifTag::GPSLatitude ||
                     en.tag == ExifTag::GPSLongitudeRef ||
-                    en.tag == ExifTag::GPSLongitude).collect();
-                println!("{:?}", dir.path());
-                for entry in entries {
-                            println!("	{}: {}",
-                            entry.tag, 
-                            entry.value);
-                }
+                    en.tag == ExifTag::GPSLongitude).map(|entry| {
+                    let tuple: (rexif::ExifTag, CoordOrValue) = match entry.value {
+                        TagValue::URational(s) => {
+                            let float_coordinates = s[0].value() + s[1].value()/60.0f64 + s[2].value()/3600.0f64;
+                            (entry.tag, CoordOrValue::Coordinate(float_coordinates))
+                        }
+                        _ => {
+                            let string_value = format!("{}", entry.value);
+                            (entry.tag, CoordOrValue::Orientation(string_value))
+                        }
+                    };
+                    tuple
+                }).collect();
+                println!("{:?}", entries);
             },
             Err(e) => {
                 print!("Error in {}: {}", &file_name,
@@ -33,12 +53,12 @@ fn scan_entries() -> Result<(), io::Error> {
             }
         }
     }
-    Ok(())
+    Ok(photos)
 }
 
 fn main() {
     match scan_entries() {
-        Ok(()) => {}
+        Ok(_) => {}
         Err(e) => panic!("Woop {}", e)
     }
 }
