2af7949851f4adf34b3a48c61061ffa650840a10
Pierre BAILLET <pierre.baillet@datadoghq.com>
2017-07-06 18:42:55 +0200

    Latest changes

---
+++ src/http_server.rs
@@ -0,0 +1,32 @@
+use std::path::Path;
+
+use iron::Iron;
+use iron::Listening;
+use staticfile::Static;
+use mount::Mount;
+
+
+pub fn start_service(ip_port: &str) -> Listening {
+    println!("Serving HTTP on: {}", ip_port);
+    Iron::new(get_mount())
+        .http(ip_port)
+        .expect("starting HTTP server FAILED")
+}
+
+fn get_mount() -> Mount {
+    // let views_handler = views::get_handler();
+    //
+    // let mut rest_chain = Chain::new(rest_api::rest_router());
+    // rest_chain.link_before(rest_api::AuthToken);
+
+    let mut mount = Mount::new();
+    mount
+        .mount(
+            "/static",
+            Static::new(Path::new("./static"))
+        );
+        // .mount("/gui-api/", gui_api::get_router())
+        // .mount("/api", rest_chain)
+        // .mount("/", views_handler);
+    mount
+}
--- src/server.rs
+++ src/server.rs
@@ -1,27 +1,12 @@
-extern crate websocket;
-extern crate hyper;
-use hyper::server::{Server, Request, Response};
-use websocket::Message;
-use websocket::sync::server::upgrade::IntoWs;
-use websocket::sync::server::upgrade::HyperRequest;
+extern crate ws;
+extern crate iron;
+extern crate staticfile;
+extern crate mount;
+
+mod http_server;
+
+use http_server::start_service;
+
 fn main() {
-    Server::http("0.0.0.0:80").unwrap().handle(move |req: Request, res: Response| {
-        match HyperRequest(req).into_ws() {
-            Ok(upgrade) => {
-                // `accept` sends a successful handshake, no need to worry about res
-                let mut client = match upgrade.accept() {
-                    Ok(c) => c,
-                    Err(_) => panic!(),
-                };
-
-                client.send_message(&Message::text("its free real estate"));
-            },
-
-            Err((request, err)) => {
-                // continue using the request as normal, "echo uri"
-                res.send(b"Try connecting over ws instead.").unwrap();
-            },
-        };
-    })
-    .unwrap();
+    start_service("0.0.0.0:8080");
 }
